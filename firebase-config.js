4bedb00e1a0f33a00ba9f0f072d67d926933b5b7
kdrolle <kdrolle594@outlook.com>
2026-02-02 12:10:36 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC0fBO_rYhdsyMf3TK67U1Y5-TNiqEfy2Q",
+    authDomain: "wackyracers-7f70c.firebaseapp.com",
+    databaseURL: "https://wackyracers-7f70c-default-rtdb.firebaseio.com",
+    projectId: "wackyracers-7f70c",
+    storageBucket: "wackyracers-7f70c.firebasestorage.app",
+    messagingSenderId: "990299606772",
+    appId: "1:990299606772:web:7f61547813ac2d45a658a1",
+    measurementId: "G-C0Z6F6CEMN"
+};
